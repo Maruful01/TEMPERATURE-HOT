fe336459d4b29b1c6621e197838bd90e815febfd
Maruful <tonmoykhan46@yahoo.com>
2021-02-05 02:28:06 +0600

    Image change added

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.getElementById ("search-btn").addEventListener ('click', function (){
         const country = data.sys.country;
         const temperature = `${weather.temp}`;
         const weatherDescription = data.weather[0].description;
+        console.log (data);
 //Kelvin to Celsius
         const kelvinToCelsius = temperature -273.15;
         const tmp = Math.round (kelvinToCelsius);
@@ -18,6 +19,36 @@ document.getElementById ("search-btn").addEventListener ('click', function (){
         document.getElementById ("country").innerText = country;
         document.getElementById ("city-name").innerText = CityName;
         document.getElementById ("description").innerText = weatherDescription;
+        
+        
+       
+// Image change
+const weatherDes = document.getElementById ("tamp").innerText;
+        function imageStyle (url) {
+                let image = document.getElementById ("body");
+                image.style.background = url;
+                image.style.backgroundSize= "cover";
+                image.style.height= "100vh";
+                image.style.objectFit= "scaleDown";
+
+        }
+        if (weatherDes <= 8) {
+
+                imageStyle ("url(images/mist.jpg) no-repeat no-repeat")
+         }
+         if (weatherDes < 1) {
+                imageStyle ("url(images/snow.jpg) no-repeat no-repeat")
+        }
+        if (weatherDes > 1) {
+                imageStyle ("url(images/cold.jpg) no-repeat no-repeat")
+        }
+        if (weatherDes > 8) {
+                imageStyle ("url(images/few-clouds.jpg) no-repeat no-repeat")
+        }
+        if (weatherDes > 16) {
+                imageStyle ("url(images/scattered-clouds.jpg) no-repeat no-repeat")
+        }
+        
     })
     }  
     currentWeather ();
